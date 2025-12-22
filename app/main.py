19e9567a055c656487e09dd822cfcb97d92c1a5c
Your Name <you@example.com>
2025-12-22 12:18:00 +0100

    Ajout du Jenkinsfile Pro avec scan de sécurité et secrets10

--- app/main.py
+++ app/main.py
@@ -2,27 +2,23 @@ from flask import Flask, send_from_directory
 import os
 import psycopg2
 
-# 1. DÉCLARER APP EN PREMIER
+# 1. On définit l'application AVANT les routes
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Affiche votre fichier HTML
     return send_from_directory('../static', 'index.html')
 
 @app.route('/db-check')
 def db_check():
     try:
-        # Tente de se connecter
+        # On utilise les noms standards PostgreSQL
         conn = psycopg2.connect(
             host=os.getenv("DB_HOST", "db"),
             database=os.getenv("POSTGRES_DB", "postgres"),
             user=os.getenv("POSTGRES_USER", "postgres"),
-            password=os.getenv("DB_PASSWORD")
+            password=os.getenv("POSTGRES_PASSWORD") # Changé pour correspondre à la DB
         )
-        cur = conn.cursor()
-        cur.close()
-        conn.close()
         return {"status": "success", "message": "Connexion à PostgreSQL réussie !"}
     except Exception as e:
         return {"status": "error", "message": str(e)}, 500
